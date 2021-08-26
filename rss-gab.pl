97c4d186eba50d58ebe1fe3580a2871943ea9f06
Vincent Vidal <VVI@d-fi.fr>
2021-08-26 16:41:28 +0200

    init

---
+++ rss-gab.pl
@@ -0,0 +1,39 @@
+use strict;
+use warnings;
+
+use DDP;
+use XML::RSS;
+use JSON::XS;
+use REST::Client;
+use 5.010;
+
+say @ARGV;
+
+# API url of the account
+my $GAB = $ARGV[0];
+# Output location, default .
+my $XML = $ARGV[1] || 'out.xml';
+
+my $client = REST::Client->new();
+my $json   = decode_json($client->GET($GAB)->responseContent());
+
+my $rss = XML::RSS->new(version => '1.0');
+$rss->channel(
+    title       => 'Title',
+    link        => $GAB,
+    description => 'Description'
+);
+
+foreach my $post ($json->@*) {
+    my $content = $post->{content} ne ''
+        ? $post->{content}
+        : $post->{reblog}->{content};
+
+    $rss->add_item(
+        title       => substr($content, 0, 40) . '...',
+        link        => $post->{url},
+        description => $content
+    );
+}
+
+$rss->save($XML);
